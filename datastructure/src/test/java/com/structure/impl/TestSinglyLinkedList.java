b6238a8f4171bf1610767aaf2c2c179ea96ee175
Mahesh Mohan Gurav <maheshgurav100@gmail.com>
2018-02-22 00:55:43 +0530

    Updated test case class

--- datastructure/src/test/java/com/structure/impl/TestSinglyLinkedList.java
+++ datastructure/src/test/java/com/structure/impl/TestSinglyLinkedList.java
@@ -1,12 +1,15 @@
 package com.structure.impl;
 
+import com.structure.LinkedList;
+import junit.framework.TestCase;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-public class SinglyLinkedListTest {
+public class TestSinglyLinkedList extends TestCase{
 
     private SinglyLinkedList<Integer> list = null;
 
@@ -22,45 +25,57 @@ public class SinglyLinkedListTest {
 
     @Test
     public void testAppendPass() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
-        assertEquals(1, list.size(), "Expected and actual values are matching");
+        assertEquals(1, list.size());
+        list = null;
     }
 
     @Test
     public void testRemoveGreaterThanPass() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
         list.removeGreaterThan(2);
         assertEquals(list.size(), 1);
         list.removeGreaterThan(0);
         assertEquals(list.size(), 0);
+        list = null;
     }
 
     @Test
     public void testRemovePass() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
         list.remove();
         assertEquals(list.size(), 0);
+        list = null;
     }
 
     @Test
     public void testAppendFail() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
         assertNotEquals(list.size(), 4);
+        list = null;
     }
 
     @Test
     public void testRemoveGreaterThanFail() {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
         list.removeGreaterThan(2);
         assertNotEquals(list.size(), 6);
         list.removeGreaterThan(0);
         assertNotEquals(list.size(), 1);
+        list = null;
     }
 
     @Test
     public void testRemoveFail() {
+        LinkedList<Integer> list = new SinglyLinkedList<Integer>();
         list.append(1);
         list.remove();
         assertNotEquals(list.size(), 2);
+        list = null;
     }
 }
